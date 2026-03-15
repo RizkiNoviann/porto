1a730e3349ca6d183ae788dd20c41e754f2d7c35
RizkiNoviann <rizkinovian236@gmail.com>
2026-03-16 00:10:09 +0800

    new commit

--- BE/src/seed/admin.seed.ts
+++ BE/src/seed/admin.seed.ts
@@ -1,4 +1,4 @@
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { PrismaService } from '../prisma/prisma.service';
 
 export async function seedAdmin(prisma: PrismaService) {
